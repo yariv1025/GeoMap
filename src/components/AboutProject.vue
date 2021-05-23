767a377c599db05175421d10d955d8dbf86b24d4
Yariv Garala <yariv1052@gmail.com>
2021-05-24 00:39:56 +0300

    Updated

--- src/components/AboutProject.vue
+++ src/components/AboutProject.vue
@@ -5,69 +5,80 @@
 
     <!-- The App Section -->
     <div class="p-3 mb-2 bg-white text-black">
-      <div class="row">
+      <div class="row" >
+
         <div class="column-66">
-          <h1 class="xlarge-font"><b>{{AppName}}</b></h1>
-          <p><span style="font-size:36px">The Gao-map is an Interactive Web
-            Mapping system that's written in JavaScript & Vue. A Web mapping
-            or an online mapping is the process of using the maps delivered by geographic information systems (GIS) on the Internet.
-            This system uses the Leaflet library and various maps are
-            embedded in it, including Google Maps and OpenStreetMap (OSM)
-            .</span>
+          <h1 class="xlarge-font"><b>What is Geo-Mapping?</b></h1>
+          <p><span style="font-size:36px">Geo-mapping is the process of taking location-based data
+            (names, addresses, sales numbers, demographic info, etc.) and
+            using it to create a map.</span>
           </p>
         </div>
+
         <div class="column-33">
           <img src="/static/img/icons/explorer5.jpg"
                class="img-thumbnail"
                width="720"
                height="480">
         </div>
+
       </div>
     </div>
 
+
     <!-- Clarity Section -->
     <div class="container-expand-lg">
-      <div class="row">
+      <div class="row" :style="{'background-image':'url(https://wallpapercave.com/wp/wp2742532.jpg'}">
+
         <div class="column-33">
           <img src="/static/img/icons/explorer3.jpg"
                class="img-thumbnail"
                width="400"
                height="560">
         </div>
+
         <div class="column-66">
-          <h1 class="xlarge-font"><b>What is Geo-Mapping?</b></h1>
-          <span style="font-size:36px">Geo-mapping is the process of taking location-based data
-            (names, ddresses, sales numbers, demographic info, etc.) and
-            using it to create a map.</span>
+          <h1 class="xlarge-font" style="color:white"><b>{{AppName}}</b></h1>
+          <span style="font-size:36px; color:white">Gao-map app is an
+            Interactive Web
+            Mapping system that's written in JavaScript & Vue. <br> A Web
+            mapping or an online mapping is the process of using the maps
+            delivered by
+            geographic information systems (GIS) on the Internet.
+            </span>
           <span class="border-bottom-0"></span>
         </div>
+
       </div>
     </div>
 
     <!-- The App Section -->
     <div class="p-3 mb-2 bg-white text-black">
       <div class="row">
+
         <div class="column-66">
           <h1 class="xlarge-font"><b>The App</b></h1>
-          <h1 class="large-font" style="color:MediumSeaGreen;"><b>Why buy
-            it?</b>
-          </h1>
-          <p><span style="font-size:36px">Take photos like a pro.</span> You
-            should buy this app because lorem ipsum consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut
-            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-            ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.</p>
-          <button class="button">Download Application</button>
+          <h1 class="large-font" style="color:MediumSeaGreen;"><b>How it's made?</b></h1>
+            <span style="font-size:36px">I chose to work
+            with Leaflet, an open source library written in JavaScript and
+            designed to display maps on websites.
+            The library is supported in various environments including mobile
+            and HTML & CSS support systems.
+            A variety of maps can be found in this project, including Google Maps
+            and OpenStreetMap (OSM).
+            </span>
+
+          <button class="button" onclick="location.href = 'http://localhost:8080/#/maps';" >Take a look</button>
         </div>
+
+
         <div class="column-33">
           <img src="/static/img/icons/explorer2.jpg"
                class="img-thumbnail"
                width="400"
                height="560">
         </div>
+
       </div>
     </div>
   </div>
@@ -121,5 +132,6 @@
       width: 100%;
       text-align: center;
     }
+
   }
 </style>
