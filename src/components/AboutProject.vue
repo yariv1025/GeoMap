2a67270d45667a93933a03c81e76a48d0af82f0a
Yariv Garala <yariv1052@gmail.com>
2021-05-23 21:53:11 +0300

    Updates

---
+++ src/components/AboutProject.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="bg-">
+
+    <!--Example: https://www.dominiquedesimpel.be/-->
+
+    <!-- The App Section -->
+    <div class="p-3 mb-2 bg-white text-black">
+      <div class="row">
+        <div class="column-66">
+          <h1 class="xlarge-font"><b>{{AppName}}</b></h1>
+          <p><span style="font-size:36px">The Gao-map is an Interactive Web
+            Mapping system that's written in JavaScript & Vue. A Web mapping
+            or an online mapping is the process of using the maps delivered by geographic information systems (GIS) on the Internet.
+            This system uses the Leaflet library and various maps are
+            embedded in it, including Google Maps and OpenStreetMap (OSM)
+            .</span>
+          </p>
+        </div>
+        <div class="column-33">
+          <img src="/static/img/icons/explorer5.jpg"
+               class="img-thumbnail"
+               width="720"
+               height="480">
+        </div>
+      </div>
+    </div>
+
+    <!-- Clarity Section -->
+    <div class="container-expand-lg">
+      <div class="row">
+        <div class="column-33">
+          <img src="/static/img/icons/explorer3.jpg"
+               class="img-thumbnail"
+               width="400"
+               height="560">
+        </div>
+        <div class="column-66">
+          <h1 class="xlarge-font"><b>What is Geo-Mapping?</b></h1>
+          <span style="font-size:36px">Geo-mapping is the process of taking location-based data
+            (names, ddresses, sales numbers, demographic info, etc.) and
+            using it to create a map.</span>
+          <span class="border-bottom-0"></span>
+        </div>
+      </div>
+    </div>
+
+    <!-- The App Section -->
+    <div class="p-3 mb-2 bg-white text-black">
+      <div class="row">
+        <div class="column-66">
+          <h1 class="xlarge-font"><b>The App</b></h1>
+          <h1 class="large-font" style="color:MediumSeaGreen;"><b>Why buy
+            it?</b>
+          </h1>
+          <p><span style="font-size:36px">Take photos like a pro.</span> You
+            should buy this app because lorem ipsum consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+            ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.</p>
+          <button class="button">Download Application</button>
+        </div>
+        <div class="column-33">
+          <img src="/static/img/icons/explorer2.jpg"
+               class="img-thumbnail"
+               width="400"
+               height="560">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AboutProject',
+    data() {
+      return {
+        AppName: 'Geo-Map',
+      }
+    }
+  }
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+
+  .container {
+    padding: 64px;
+  }
+
+  /* Clear floats */
+  .row:after {
+    content: "";
+    display: table;
+    clear: both
+  }
+
+  /* 2/3 column */
+  .column-66 {
+    float: left;
+    width: 66.66666%;
+    padding: 20px;
+  }
+
+  /* 1/3 column */
+  .column-33 {
+    float: left;
+    width: 33.33333%;
+    padding: 20px;
+  }
+
+  /* Add responsiveness - make the columns appear on top of each other instead of next to each other on small screens */
+  @media screen and (max-width: 1000px) {
+    .column-66,
+    .column-33 {
+      width: 100%;
+      text-align: center;
+    }
+  }
+</style>
---
+++ src/components/Topbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">{{projectName}}</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent" aria-expanded="false"
+              aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
+            style="--bs-scroll-height: 100px;">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#/maps">Maps</a>
+          </li>
+        </ul>
+      </div>
+
+<!--      <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
+<!--        <form class="d-flex">-->
+<!--          <input class="form-control me-2" type="search"-->
+<!--                 placeholder="To Active Search"-->
+<!--                 aria-label="Search">-->
+<!--          <button class="btn btn-outline-success" type="submit">Search</button>-->
+<!--        </form>-->
+<!--      </div>-->
+    </div>
+  </nav>
+
+</template>
+
+<script>
+  export default {
+    name: 'Navbar',
+
+    data() {
+      return {
+        projectName: 'Geo-Map'
+      }
+    },
+
+  }
+</script>
+
