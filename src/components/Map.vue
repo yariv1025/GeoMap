f8a629a2af519f5c3f8a76774738f49c87d0e7e9
Yariv Garala <yariv1052@gmail.com>
2021-05-30 16:02:00 +0300

    Updated: change leaflet search bar

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -30,18 +30,6 @@
             </div>
           </div>
 
-
-<!--                    <button type="submit"-->
-<!--                            value="Send"-->
-<!--                            class="btn btn-secondary btn-lg"-->
-<!--                            v-on:click = "addMarker">-->
-<!--                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up" viewBox="0 0 16 16">-->
-<!--                        <path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1h-2z"/>-->
-<!--                        <path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 1.707V10.5a.5.5 0 0 1-1 0V1.707L5.354 3.854a.5.5 0 1 1-.708-.708l3-3z"/>-->
-<!--                      </svg>-->
-<!--                      Upload marker-->
-<!--                    </button>-->
-
         </div>
       </div>
 
@@ -259,7 +247,7 @@
         const provider = new OpenStreetMapProvider()
         const searchControl = new GeoSearchControl({
           provider: provider,
-          style: 'bar',
+          // style: 'bar',
           autoComplete: true,
           autoCompleteDelay: 250, // A little delay is required to not DDOS the server on every keystroke.
           showMarker: true, // optional: true|false  - default true
